ab9a170279cbbb9a053b617f85175222364d0dc2
wjx <2457359166@qq.com>
2023-10-31 22:57:14 +0800

    第一次提交

---
+++ 生日礼物（爱心）.py
@@ -0,0 +1,37 @@
+import turtle as t
+
+
+print("生日快乐！")
+#定义star函数
+def star(p,s,c,x,y):
+     t.goto(x,y)
+     t.pendown()
+     a=180-(180/p)
+     t.color(c)
+     for i in range(p):
+        t.forward(s)
+        t.right(a)
+
+  
+
+
+
+#用star函数来画爱心
+t.penup()
+star(5,20,"red",0,50)
+star(5,20,"blue",50,100)
+star(5,20,"red",100,50)
+star(5,20,"blue",100,0)
+star(5,20,"red",0,-100)
+star(5,20,"blue",-100,0)
+star(5,20,"red",-100,50)
+star(5,20,"blue",-50,100)
+t.pendown()
+t.color("red")
+t.goto(0,50)
+t.penup()
+t.goto(0,0)
+t.write("I love you",)
+t.penup()
+    
+    
---
+++ 解方程.py
@@ -0,0 +1,11 @@
+
+x=0
+
+while True:
+    if 2*x+5==19:
+        print(x)
+        break
+    else:
+        x=x+1
+    
+    
